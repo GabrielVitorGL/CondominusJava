20faf6d4a59f00f8e92ca107fcc5ad7984e2abef
VitorCdSouza <vitorcorddesouza@gmail.com>
2023-08-22 09:47:22 -0300

    Criacao projeto

---
+++ src/main/java/br/com/etechoracio/condominus/CondominusApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.condominus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/condominus/CondominusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.condominus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
